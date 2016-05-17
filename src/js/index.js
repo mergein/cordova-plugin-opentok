4c27638d7ddf356bbf2de4d3e959056eac3559bf
andrewdhazlett <andrew@hazlett.me>
2016-05-17 15:54:15 -0400

    convert OT.coffee to es2015

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,83 @@
-console.log('hello, world!');
+/* globals Cordova, OTHelpers, OTPlugin, pdebug, TBError, TBPublisher, TBSession, TBUpdateObjects */
+
+export const OT = {
+  checkSystemRequirements() {
+    return 1;
+  },
+
+  initPublisher(targetElement, properties, completionHandler) {
+    return new TBPublisher(targetElement, properties, completionHandler);
+  },
+
+  initSession(apiKey, sessionId) {
+    if (!sessionId) {
+      return this.showError('OT.initSession takes 2 parameters, your API Key and Session ID');
+    }
+    return new TBSession(apiKey, sessionId);
+  },
+
+  log(message) {
+    return pdebug('TB LOG', message);
+  },
+
+  off(/* event, handler */) {
+    // TODO
+  },
+
+  on(event, handler) {
+    // TB object only dispatches one type of event
+    if (event === 'exception') {
+      console.log('JS: TB Exception Handler added');
+      Cordova.exec(handler, TBError, OTPlugin, 'exceptionHandler', []);
+    }
+  },
+
+  setLogLevel() {
+    return console.log('Log Level Set');
+  },
+
+  setErrorCallback(callback) {
+    this.errorCallback = callback;
+  },
+
+  upgradeSystemRequirements() {
+    return {};
+  },
+
+  updateViews() {
+    return TBUpdateObjects();
+  },
+
+  // helpers
+  getHelper() {
+    if (typeof window.jasmine === 'undefined' || !window.jasmine || !window.jasmine.getEnv) {
+      window.jasmine = {
+        getEnv() { return; }
+      };
+    }
+    this.OTHelper = this.OTHelper || OTHelpers.noConflict();
+    return this.OTHelper;
+  },
+
+  // deprecating
+  showError(a) {
+    alert(a);
+  },
+
+  addEventListener(event, handler) {
+    this.on(event, handler);
+  },
+
+  removeEventListener(type, handler) {
+    this.off(type, handler);
+  }
+};
+
+window.TB = window.OT = OT;
+
+window.addEventListener('orientationchange', () => {
+  setTimeout(() => {
+    OT.updateViews();
+    return;
+  }, 1000);
+}, false);
